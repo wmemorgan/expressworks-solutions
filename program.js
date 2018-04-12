b739b88266a6501a695e357af987d707b43565e5
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-12 17:51:09 +0000

    STATIC Exercise 2 of 8  On branch ex02  Changes to be committed: 	modified:   program.js

--- program.js
+++ program.js
@@ -1,15 +1,11 @@
-// HELLO WORLD! Exercise 1 of 8
+// STATIC Exercise 2 of 8
 
-// const express = require('express')
-// const app = express()
-// app.get('/home', (req, res) => {
-//   res.end('Hello World!')
-// })
-// app.listen(process.argv[2])
-
-var express = require('express')
-var app = express()
-app.get('/home', function(req, res) {
+const express = require('express')
+const app = express()
+app.get('/home', (req, res) => {
   res.end('Hello World!')
 })
+
+app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
+
 app.listen(process.argv[2])
