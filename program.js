ea39884bc95145e2f81c48ebbced605e9cd4ef25
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-12 17:44:02 +0000

    HELLO WORLD! Exercise 1 of 8 [My Solution] On branch ex01 Changes to be committed: 	modified:   program.js

--- program.js
+++ program.js
@@ -1,15 +1,9 @@
 // HELLO WORLD! Exercise 1 of 8
+// My Solution
 
-// const express = require('express')
-// const app = express()
-// app.get('/home', (req, res) => {
-//   res.end('Hello World!')
-// })
-// app.listen(process.argv[2])
-
-var express = require('express')
-var app = express()
-app.get('/home', function(req, res) {
+const express = require('express')
+const app = express()
+app.get('/home', (req, res) => {
   res.end('Hello World!')
 })
 app.listen(process.argv[2])
