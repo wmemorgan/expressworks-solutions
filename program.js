b9d8eae120792750152ccb59a7fbab9b866147b6
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-13 06:09:32 +0000

    Add program logic notes On branch ex06 Changes to be committed: 	modified:   program.js

--- program.js
+++ program.js
@@ -1,4 +1,28 @@
-// STYLISH CSS Exercise 5 of 8
+// PARAM PAM PAM Exercise 6 of 8
+
+//Load and activate express module
+const express = require('express'),
+app = express();
+
+app.listen(process.argv[2])
+
+/*-------NOTES----------
+1. Load and activate express server
+PUT /message/526aa677a8ceb64569c9d4fb
+
+2. Load and activate crypto module
+require('crypto')
+  .createHash('sha1')
+  .update(new Date().toDateString() + id)
+  .digest('hex')
+
+3. Extract parameters from within the request handlers
+app.put('/path/:NAME', function(req, res){...});
+
+*/
+
+/*-------PREVIOUS SOLUTIONS----
+//STYLISH CSS Exercise 5 of 8
 
 //Load and activate express module
 const express = require('express'),
@@ -12,3 +36,4 @@ app.use(express.static(process.argv[3]));
 
 app.listen(process.argv[2])
 
+*/
