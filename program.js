480e66d890af2b5f1b66be5410a708e250f83820
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-13 05:53:23 +0000

    STYLISH CSS Exercise 5 of 8 [My Solution] On branch ex05 Changes to be committed: 	modified:   program.js

--- program.js
+++ program.js
@@ -12,16 +12,3 @@ app.use(express.static(process.argv[3]));
 
 app.listen(process.argv[2])
 
-/* NOTES
-1.Install stylus module
-npm install stylus
-
-2.Activate stylus middleware
-app.use(require('stylus').middleware(__dirname + '/public'));
-
-3.Serve static HTML files pointing to a directory path provided by
-the parameter process.argv[3]
-
-4.Listen on the port number supplied by process.argv[2].
-
-*/
