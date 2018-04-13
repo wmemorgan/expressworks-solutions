6a1458d6c4079ed9b6b7206ec756ee780f44741b
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-13 05:20:32 +0000

    STYLISH CSS Exercise 5 of 8
     On branch ex05
     Changes to be committed:
    	modified:   program.js
    	added program logic notes

--- program.js
+++ program.js
@@ -1,4 +1,4 @@
-// GOOD OLD FORM Exercise 4 of 8
+// STYLISH CSS Exercise 5 of 8
 
 const express = require('express'),
 bodyparser = require('body-parser'),
@@ -11,3 +11,17 @@ app.post('/form', (req, res) => {
 })
 
 app.listen(process.argv[2])
+
+/* NOTES
+1.Install stylus module
+npm install stylus
+
+2.Activate stylus middleware
+app.use(require('stylus').middleware(__dirname + '/public'));
+
+3.Serve static HTML files pointing to a directory path provided by
+the parameter process.argv[3]
+
+4.Listen on the port number supplied by process.argv[2].
+
+*/
