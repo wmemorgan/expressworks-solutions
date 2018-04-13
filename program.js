5ed1abb300fa1caf97a63c9baca28b34b7825fa4
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-13 05:52:17 +0000

    STYLISH CSS Exercise 5 of 8 (Beta) On branch ex05  Changes to be committed: 	modified:   program.js 	added comments

--- program.js
+++ program.js
@@ -1,9 +1,13 @@
 // STYLISH CSS Exercise 5 of 8
 
+//Load and activate express module
 const express = require('express'),
 app = express();
 
+//Load and activate stylus middleware
 app.use(require('stylus').middleware(process.argv[3]));
+
+//Serve static HTML files
 app.use(express.static(process.argv[3]));
 
 app.listen(process.argv[2])
@@ -19,56 +23,5 @@ app.use(require('stylus').middleware(__dirname + '/public'));
 the parameter process.argv[3]
 
 4.Listen on the port number supplied by process.argv[2].
-
-*/
-
-/* Previous solutions
-// HELLO WORLD! Exercise 1 of 8
-// My Solution
-
-const express = require('express')
-const app = express()
-app.get('/home', (req, res) => {
-  res.end('Hello World!')
-})
-app.listen(process.argv[2])
-
-// STATIC Exercise 2 of 8
-
-const express = require('express')
-const app = express()
-app.get('/home', (req, res) => {
-  res.end('Hello World!')
-})
-
-app.use(express.static(process.argv[3]||path.join(__dirname, 'public')));
-
-app.listen(process.argv[2])
-
-// JADE Exercise 3 of 8
-
-const express = require('express')
-const app = express()
-
-app.set('views', process.argv[3]||path.join(__dirname, 'templates'))
-app.set('view engine', 'jade')
-app.get('/home', (req, res) => {
-  res.render('index', {date: new Date().toDateString()})
-})
-app.listen(process.argv[2])
-
-// GOOD OLD FORM Exercise 4 of 8
-
-const express = require('express'),
-bodyparser = require('body-parser'),
-app = express();
-
-app.use(bodyparser.urlencoded({extended: false}))
-
-app.post('/form', (req, res) => {
-    res.end(req.body.str.split('').reverse().join(''));
-})
-
-app.listen(process.argv[2])
 
 */
