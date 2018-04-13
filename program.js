d708335dec235b9a6f905c8985eb2ee24aef080c
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-13 07:43:39 +0000

    Add program logic notes

--- program.js
+++ program.js
@@ -1,3 +1,31 @@
+// WHAT'S IN QUERY Exercise 7 of 8
+
+//Load and activate express module
+const express = require('express'),
+app = express();
+
+});
+
+app.listen(process.argv[2])
+
+/*-------NOTES----------
+Write a route that extracts data from query string in the GET '/search' URL
+route, e.g. ?results=recent&include_tabs=true and then outputs it back to
+the user in JSON format.
+
+1. Load and activate express server
+
+2. Write a route in the GET '/search' directory
+
+3. Extract query string parameters use:
+    req.query.NAME
+    
+4. Return the string parameters in JSON format. We can use:
+    res.send(object)
+*/
+
+
+/*-------Previous Solution-------
 // PARAM PAM PAM Exercise 6 of 8
 
 //Load and activate express module
@@ -16,26 +44,4 @@ app.put('/message/:id', (req, res) => {
      
     res.end(idHash);
 });
-
-app.listen(process.argv[2])
-
-/*-------NOTES----------
-Create an Express.js server that processes PUT '/message/:id' requests.
-
-For instance:
-    PUT /message/526aa677a8ceb64569c9d4fb
-
-1. Load and activate express server
-app.put('/path/:NAME', function(req, res){...});
-
-2. Extract parameters from within the request handlers. 
-To extract parameters from within the request handlers, use:
-    req.params.NAME
-    
-3. Transform and return the request message ID to SHA1 hash   
-require('crypto')
-  .createHash('sha1')
-  .update(new Date().toDateString() + id)
-  .digest('hex')
-
 */
