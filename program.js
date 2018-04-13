22eb72aed1982e6fb2477dd0af41fb3fcd8ce8b0
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-13 09:01:39 +0000

    JSON ME Exercise 8 of 8 [My Solution with Notes] On branch ex08  Changes to be committed: 	modified:   program.js

--- program.js
+++ program.js
@@ -6,6 +6,18 @@ app = express(),
 //Load and activate the fs module
 fs = require('fs');
 
+//Create a route in the GET '/books' directory
+app.get('/books', (req, res) => {
+    //Read the file using the fs module
+    fs.readFile(process.argv[3], (err, data) => {
+        if (err) throw err;
+        //Parse the file content.
+        var content = JSON.parse(data);
+        //Return the content in JSON format.
+        res.json(content);
+    })
+    
+})
 
 app.listen(process.argv[2])
 
@@ -13,35 +25,18 @@ app.listen(process.argv[2])
 Write a server that reads a file, parses it to JSON and outputs the content
 to the user.
 
-1. Load and activate express server
+1. Load and activate express server (DONE)
 
-2. Load and activate the fs module
+2. Load and activate the fs module (DONE)
 
-3. Create a route in the GET '/books' directory
+3. Create a route in the GET '/books' directory (DONE)
 
-4. Read the file using the fs module. We can use:
+4. Read the file using the fs module. We can use: (DONE)
     fs.readFile(process.argv[3], callback)
     
-5. Parse the file and return the content in JSON format. We can use:
+5. Parse the file content. We can use: (DONE)
     object = JSON.parse(content)
-*/
-
-
-/*--------Previous Solution------------
-// WHAT'S IN QUERY Exercise 7 of 8
-
-//Load and activate express module
-const express = require('express'),
-app = express();
-
-//Create a route in the GET '/search' directory
-app.get('/search', (req, res) => {
-    //Extract query string parameters and 
-    //return in JSON format
-    res.send(req.query);
-})
-
-app.listen(process.argv[2])
-
-
+    
+6. Return the content in JSON format. Respond with: (DONE)
+   res.json(content)
 */
