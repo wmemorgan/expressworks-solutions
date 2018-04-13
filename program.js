cd8362f818fa3dc295cce4ea33699e1e0fc9501d
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-13 08:02:21 +0000

    WHAT'S IN QUERY Exercise 7 of 8 [My Solution with Notes] On branch ex07 Changes to be committed: 	modified:   program.js

--- program.js
+++ program.js
@@ -4,7 +4,12 @@
 const express = require('express'),
 app = express();
 
-});
+//Create a route in the GET '/search' directory
+app.get('/search', (req, res) => {
+    //Extract query string parameters and 
+    //return in JSON format
+    res.send(req.query);
+})
 
 app.listen(process.argv[2])
 
@@ -15,33 +20,11 @@ the user in JSON format.
 
 1. Load and activate express server
 
-2. Write a route in the GET '/search' directory
+2. Create a route in the GET '/search' directory
 
 3. Extract query string parameters use:
     req.query.NAME
     
 4. Return the string parameters in JSON format. We can use:
     res.send(object)
-*/
-
-
-/*-------Previous Solution-------
-// PARAM PAM PAM Exercise 6 of 8
-
-//Load and activate express module
-const express = require('express'),
-app = express();
-
-app.put('/message/:id', (req, res) => {
-    //Extract parameters from within the request handlers
-    var id = req.params.id;
-   
-    //Transform and return the request message ID to SHA1 hash   
-    var idHash = require('crypto')
-        .createHash('sha1')
-        .update(new Date().toDateString() + id)
-        .digest('hex')
-     
-    res.end(idHash);
-});
 */
