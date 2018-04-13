5efec99b16e24ac80fe7729edf9dedf9e3050416
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-04-13 08:14:18 +0000

    Add program logic notes

--- program.js
+++ program.js
@@ -1,3 +1,33 @@
+// JSON ME Exercise 8 of 8
+
+//Load and activate express module
+const express = require('express'),
+app = express(),
+//Load and activate the fs module
+fs = require('fs');
+
+
+app.listen(process.argv[2])
+
+/*-------NOTES----------
+Write a server that reads a file, parses it to JSON and outputs the content
+to the user.
+
+1. Load and activate express server
+
+2. Load and activate the fs module
+
+3. Create a route in the GET '/books' directory
+
+4. Read the file using the fs module. We can use:
+    fs.readFile(process.argv[3], callback)
+    
+5. Parse the file and return the content in JSON format. We can use:
+    object = JSON.parse(content)
+*/
+
+
+/*--------Previous Solution------------
 // WHAT'S IN QUERY Exercise 7 of 8
 
 //Load and activate express module
@@ -13,18 +43,5 @@ app.get('/search', (req, res) => {
 
 app.listen(process.argv[2])
 
-/*-------NOTES----------
-Write a route that extracts data from query string in the GET '/search' URL
-route, e.g. ?results=recent&include_tabs=true and then outputs it back to
-the user in JSON format.
 
-1. Load and activate express server
-
-2. Create a route in the GET '/search' directory
-
-3. Extract query string parameters use:
-    req.query.NAME
-    
-4. Return the string parameters in JSON format. We can use:
-    res.send(object)
 */
